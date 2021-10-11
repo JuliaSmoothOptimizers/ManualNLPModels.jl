71bb2752b705f0a03ee618c19c5b4b13f38cb654
abelsiqueira <abelsiqueira@users.noreply.github.com>
2021-10-11 19:23:22 +0000

    :robot: Format .jl files

--- test/problems/hs5.jl
+++ test/problems/hs5.jl
@@ -16,8 +16,12 @@ export hs5
 Starting point: `[0.0; 0.0]`.
 """
 function hs5(::Type{T} = Float64) where {T}
-  hprod(hv, x, v; obj_weight=one(T)) = (hv .= (-sin(x[1] + x[2]) * (v[1] + v[2]) * ones(T, 2) + 2 * [v[1] - v[2]; v[2] - v[1]]) * obj_weight)
-  hess_coord(vals, x; obj_weight=one(T)) = begin
+  hprod(hv, x, v; obj_weight = one(T)) = (
+    hv .=
+      (-sin(x[1] + x[2]) * (v[1] + v[2]) * ones(T, 2) + 2 * [v[1] - v[2]; v[2] - v[1]]) *
+      obj_weight
+  )
+  hess_coord(vals, x; obj_weight = one(T)) = begin
     vals[1] = vals[3] = -sin(x[1] + x[2]) + 2
     vals[2] = -sin(x[1] + x[2]) - 2
     vals .*= obj_weight
@@ -27,7 +31,8 @@ function hs5(::Type{T} = Float64) where {T}
     T[-1.5; -3],
     T[4; 3],
     x -> sin(x[1] + x[2]) + (x[1] - x[2])^2 - 3x[1] / 2 + 5x[2] / 2 + 1,
-    grad = (gx, x) -> (gx .= cos(x[1] + x[2]) * ones(T, 2) + 2 * (x[1] - x[2]) * T[1; -1] + T[-1.5; 2.5]),
+    grad = (gx, x) ->
+      (gx .= cos(x[1] + x[2]) * ones(T, 2) + 2 * (x[1] - x[2]) * T[1; -1] + T[-1.5; 2.5]),
     hprod = hprod,
     hess_coord = ([1, 2, 2], [1, 1, 2], hess_coord),
   )
